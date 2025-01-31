fc2ea26b3cf800d8f714b24f9b546d93317f6439
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-31 08:22:11 -0700

    Header and first section of math code.

--- Better_Calculator/BetterCalculator.vb
+++ Better_Calculator/BetterCalculator.vb
@@ -1,6 +1,35 @@
-﻿Module BetterCalculator
+﻿'Payden Hoskins
+'RCET2265
+'Spring 2025
+'Better Calculator
+
+Option Explicit On
+Option Strict On
+
+
+Module BetterCalculator
 
     Sub Main()
+        Dim FirstNumber As Integer
+        Dim SecondNumber As Integer
+        Dim UserInput As String
+
+        Do
+            Console.WriteLine("Type 'Q' at any point to Quit")
+            Console.WriteLine("Enter 1st Number: ")
+            UserInput = Console.ReadLine()
+            Console.WriteLine($"You entered {UserInput}.")
+            Try
+                FirstNumber = CInt(UserInput)
+            Catch ex As Exception
+                If UserInput <> "Q" Then
+                    Console.WriteLine($"{UserInput} is not a whole number...")
+                End If
+            End Try
+
+
+
+        Loop While UserInput <> "Q"
 
     End Sub
 
